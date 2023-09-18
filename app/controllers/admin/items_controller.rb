247cee02bc416563b7d2feabcf30df97ed3ddd07
Yurino Naito <yurino.naito@gmail.com>
2023-09-18 13:42:30 +0000

    管理者の商品更新画面の作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,9 +6,22 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    @genres = Genre.all
+    @item.save
+    redirect_to admin_items_path
+  end
+
   def show
   end
 
   def edit
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :image, :caption, :price)
   end
 end
