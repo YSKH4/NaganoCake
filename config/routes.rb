108f5a9faae20240ecab9acfdd390df795d07bbd
haruto <138562348+haru-shducks@users.noreply.github.com>
2023-09-18 07:45:05 +0000

    add association

--- config/routes.rb
+++ config/routes.rb
@@ -1,62 +1,68 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'orders/show'
+    resources :orders,only: [:show, :update]
   end
+  
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
+    esources :customers, only: [:index, :show, :edit, :update]
   end
+  
   namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
+    resources :genres, only: [:index, :create, :edit, :update]
   end
+  
   namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
+  
   namespace :admin do
     get 'homes/top'
   end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
+  
+  scope module: :public do
+    resources :addresses,only: [:index, :edit, :create, :update, :destroy]
   end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/thanx'
-    get 'orders/index'
-    get 'orders/show'
+  
+  scope module: :public do
+    resources :orders, only: [:new, :create, :index, :show]
+    get 'orders/thanx' => 'orders#thanx'
+    post 'orders/comfirm' => 'orders#comfirm'
   end
-  namespace :public do
-    get 'cart_items/index'
+  
+  scope module: :public do
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
   end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/confirm_withdraw'
+  
+  scope module: :public do
+    get 'customers/mypage' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/confirm_withdraw' => 'customers#confirm_withdraw'
+    patch 'customers/withdraw' => 'customers#withdraw'
   end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
+  
+  scope module: :public do
+    resources :items,only: [:index, :show]
   end
-  namespace :public do
-    get 'homes/top'
+  
+  scope module: :public do
+    root to:"homes#top"
     get 'homes/about'
   end
+  
+  
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
+  registrations: 'public/registrations',
   sessions: 'public/sessions'
 }
 
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
+  sessions: 'admin/sessions'
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
