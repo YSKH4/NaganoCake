d1ac6c08b46b45468534a1c93f20ad7597c423d3
haruto <138562348+haru-shducks@users.noreply.github.com>
2023-09-18 06:03:12 +0000

    controller g all

--- db/schema.rb
+++ db/schema.rb
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 2023_09_18_042913) do
     t.string "name", null: false
     t.text "caption", null: false
     t.integer "price", null: false
-    t.boolean "is_active", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
