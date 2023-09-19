d679b8c048dd07c33b125dea05ec0a5fa507700d
Yurino Naito <yurino.naito@gmail.com>
2023-09-19 02:33:18 +0000

    管理者の顧客機能を追加・バリデーション途中まで

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,31 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_customer, only: %i[index show edit update]
+
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Costomer.find(params[:id])
   end
 
   def edit
   end
+  
+  def update
+    @customer.update(customer_params)
+    redirect_to request.referer, notice: 'Successfully updated customer status'
+  end
+  
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:status)
+  end
+  
 end
