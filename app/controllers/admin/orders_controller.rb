a11c51cc1fbc564973445c7b6f63ad077aa6142f
Yurino Naito <yurino.naito@gmail.com>
2023-09-20 09:35:25 +0000

    管理者の注文詳細画面の作成・enumまで

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = @item= Item.find(params[:id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,30 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+  
+  def full_name
+    "#{last_name}#{first_name}"
+  end
+  
+  def full_address
+    "#{zip_code}#{shipping_address}#{shipping_name}"
+  end
+  
+  enum payment_method: { transfer: 0, credit_card: 1 }
+  enum status: {unpaid: 1, payment_check: 2, in_production: 3, preparing_to_ship: 4, shipped: 5 }
+  
+  def payment_method_text
+     if payment_method == 0
+      Order.payment_methods_i18n[:credit_card]
+     else
+      Order.payment_methods_i18n[:transfer]
+     end
+  end
+     
+   def status_text
+     if status == 0
+      Order.statuses_i18n[:unpaid]
+     end
+   end
+
 end
