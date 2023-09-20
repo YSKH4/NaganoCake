af284e99ae056656fb9f2055db6ff8b2a9d5cde5
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-09-20 03:20:01 +0000

    会員登録(修正必要）、ログイン、ログアウト、マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,19 @@
 class Public::CustomersController < ApplicationController
+
   def show
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
+    
   end
 
   def confirm_withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :password, :remember_me, :first_name, :last_name, :last_name_kana, :first_name_kana, :zip_code, :address, :telephone_number)
   end
 end
