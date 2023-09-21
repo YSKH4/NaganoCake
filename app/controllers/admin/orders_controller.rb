a32bc0a0bd589291c1887307285d0e221af85179
Yurino Naito <yurino.naito@gmail.com>
2023-09-21 13:45:34 +0000

    管理 者注文詳細画面作成・起動確認待ち・更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,19 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
   end
+  
+  def update
+    update 
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    if @order.update(order_params)
+       @order_details.update_all(making_status: "制作待ち") if @order.status == "入金確認"
+    end
+      redirect_to admin_order_path(@order)
+  end
+  
+  def order_params
+    params.require(:order).permit(:payment_method, :postage, :billing_amount, :status, :zip_code, :shipping_address, :shipping_nameamount)
+  end
+  
 end
