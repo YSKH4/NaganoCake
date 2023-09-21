31a62102266b4afa1a9e078c83e638bbc1400c2c
Hiroki-516 <141198036+Hiroki-516@users.noreply.github.com>
2023-09-21 07:08:24 +0000

    admin-rogin

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,7 +1,26 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Oder.new
-    @addresses = Address.
+    @addresses = Address.all
+  end
+  
+  def confirm
+     @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.zip_code = current_customer.zip_code
+      @order.shipping_address = current_customer.shipping_address
+      @order.shipping_name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+       @address = Address.find(params[:order][:address_id])
+       @order.zip_code = @address.zip_code
+       @order.shipping_address = @address.shipping_address
+       @order.shipping_name = @address.shipping_name
+    elsif params[:order][:select_address] == "2"
+      @order.customer_id = current_customer.id
+    end
+      @cart_items = current_customer.cart_items
+      @order_new = Order.new
+      render :confirm
   end
 
   def thanx
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :customer
+  
+  def full_address
+    '〒' + zip_code + ' ' + shipping_address + ' ' + shipping_name
+  end
+  
+  
 end
