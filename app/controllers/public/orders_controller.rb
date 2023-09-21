4edcd88e7d564b452894679ee10426ff81c32791
Hiroki-516 <141198036+Hiroki-516@users.noreply.github.com>
2023-09-21 06:18:36 +0000

    Nofeature-address,order-system

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,7 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Oder.new
+    @addresses = Address.
   end
 
   def thanx
--- app/models/oder_detail.rb
+++ app/models/oder_detail.rb
@@ -1,4 +1,14 @@
 class OderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+  
+
+  enum manufacture_status:
+        {
+          impossible_manufacture:0,
+          waiting_manufacture:1,
+          manufacturing:2,
+          finish:3
+        }
+
 end
