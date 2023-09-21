f7e421c06ff3ad3ea2ccfbe91b6b302d124956c7
Yurino Naito <yurino.naito@gmail.com>
2023-09-21 06:26:14 +0000

    商品投稿機能と管理者顧客機能のBootstrap追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -18,11 +18,11 @@ class Customer < ApplicationRecord
   validates :telephone_number, presence: true
 
   def full_name
-    "#{first_name} #{last_name}"
+    "#{last_name} #{first_name}"
   end
 
   def full_name_kana
-    "#{first_name_kana} #{last_name_kana}"
+    "#{last_name_kana} #{first_name_kana}"
   end
 
 
