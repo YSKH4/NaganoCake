bf4569f666f763aa0cefe608e1728c6a7b834757
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-09-21 08:22:50 +0000

    MERGE用

--- db/schema.rb
+++ db/schema.rb
@@ -75,14 +75,14 @@ ActiveRecord::Schema.define(version: 2023_09_21_074247) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.text "zip_code"
-    t.text "address"
-    t.string "telephone_number"
-    t.boolean "is_deleted"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.text "zip_code", null: false
+    t.text "address", null: false
+    t.string "telephone_number", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
