6de6b3e08870d58917af95e924850829e4c76cad
Yurino Naito <yurino.naito@gmail.com>
2023-09-21 01:05:47 +0000

    日本語表記追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,19 +12,21 @@ class Order < ApplicationRecord
   
   attribute :last_name, :string
   attribute :first_name, :string
-  
+  #フルネーム
   def full_name
     "#{last_name}#{first_name}"
   end
-  
+  #宛先全ての情報
   def full_address
     "#{zip_code}#{shipping_address}#{shipping_name}"
   end
   
+  #enum
   enum payment_method: { transfer: 0, credit_card: 1 }
   enum status: {unpaid: 1, payment_check: 2, in_production: 3, preparing_to_ship: 4, shipped: 5 }
   # enum making_status: { untouched: 0, waiting_for_production: 1, in_production: 2, production_completed: 3 }
   
+  #支払方法
   def payment_method_text
      if payment_method == 0
       Order.payment_methods_i18n[:credit_card]
