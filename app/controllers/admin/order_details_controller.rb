1943a0553ab7abb21dde343143c361c829a59391
Yurino Naito <yurino.naito@gmail.com>
2023-09-23 08:05:03 +0000

    ステータス連動機能修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,28 +1,49 @@
 class Admin::OrderDetailsController < ApplicationController
   
-  def update 
+#   def update 
+#     @order_detail = OrderDetail.find(params[:id])
+#     # @order = @order_detail.order
+#     @order = Order.find(params[:order_id])
+#     @order_details = @order.order_details.all
+    
+#   is_updated = true
+#     if @order_detail.update(order_detail_params)
+#       @order.update(status: "制作中") if @order_detail.making_status == "制作中"
+#       # ②製作ステータスが「製作中」のときに、注文ステータスを「製作中」に更新する。
+#       @order_details.each do |order_detail|  # ③紐付いている注文商品の製作ステータスが "すべて" [製作完了]になった際に注文ステータスを「発送準備中」に更新
+#           if order_detail.making_status != "制作完了"　#紐付いている注文商品の製作ステータスを一つ一つeach文で確認
+#           # 製作ステータスが「製作完了」ではない場合
+#             is_updated = false 　# 上記で定義してあるis_updatedを「false」に変更する。
+#           end
+#       end
+#       @order.update(status: "発送準備中") if is_updated　# is_updatedがtrueの場合に、注文ステータスが「発送準備中」に更新されます。上記のif文でis_updatedがfalseになっている場合、更新されません。
+#     end
+#       redirect_to admin_order_path(@order)
+# end
+
+#   private
+
+#   def order_detail_params
+#     params.require(:order_detail).permit(:making_status, )
+#   end
+
+def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
     @order_details = @order.order_details.all
-   #@order_detail.update(order_detail_params)
     
-   is_updated = true
     if @order_detail.update(order_detail_params)
-       @order.update(status: "制作中") if @order_detail.making_status == "制作中"
-       @order_details.each do |order_detail| 
-          if order_detail.making_status != "制作完了"
-            is_updated = false 
-          end
-       end
-       @order.update(status: "発送準備中") if is_updated
+      @order.update_status_on_order_detail_change
+      redirect_to admin_order_path(@order)
+    else
+      render :show
     end
-       redirect_to admin_order_path(@order)
-end
-   private
+  end
+  
+  private
 
   def order_detail_params
-    params.require(:order_detail).permit(:amount, :unit_price, :making_statusname, )
+    params.require(:order_detail).permit(:making_status)
   end
   
 end
-
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,18 +5,40 @@ class Admin::OrdersController < ApplicationController
 
   end
   
+  # def update
+  #   @order = Order.find(params[:id])
+  #   @order_details = @order.order_details
+  #   if @order.update(order_params)
+  #     @order_details.update_all(making_status: "制作待ち") if @order.status == "入金確認"
+  #       ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+  #   end
+  #     redirect_to admin_order_path(@order)
+  # end
+  
+  # def order_params
+  #   params.require(:order).permit(:status)
+  # end
+  
   def update
-    update 
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+
     if @order.update(order_params)
-       @order_details.update_all(making_status: "制作待ち") if @order.status == "入金確認"
-    end
+      if @order_details.all? { |detail| detail.making_status == '制作完了' }
+        @order.update(status: '発送準備中')
+      elsif @order_details.any? { |detail| detail.making_status == '制作中' }
+        @order.update(status: '制作中')
+      end
       redirect_to admin_order_path(@order)
+    else
+      render :show
+    end
   end
   
+  private
+
   def order_params
-    params.require(:order).permit(:payment_method, :postage, :billing_amount, :status, :zip_code, :shipping_address, :shipping_nameamount)
+    params.require(:order).permit(:status)
   end
   
 end
